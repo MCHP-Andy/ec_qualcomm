c4c4870343658c38b39dad2d8171399cb8ad76eb
MCHP-Andy <andy.chang@microchip.com>
2026-02-10 14:54:36 +0800

    feat: setup env for mec5

--- tools/script/setup.bat
+++ tools/script/setup.bat
@@ -6,7 +6,8 @@ set ZEPHYR_TOOLCHAIN_VARIANT=zephyr
 
 set WORKSPACE=%cd%
 set ZEPHYR_SDK_INSTALL_DIR=%WORKSPACE%\zephyr-sdk-0.16.8
-set MEC5_SPI_GEN=%WORKSPACE%\tools\CPGZephyrDocs\MEC175x\SPI_image_gen\mec175x_spi_gen.exe
+set MEC5_SPI_GEN=%WORKSPACE%\tools\mec175x_spi_gen.exe
+set MEC5_SPI_CFG=%WORKSPACE%\app\boards\support\mec175x_spi_cfg.txt
 set EC_IMG_GEN=%WORKSPACE%\tools\spi_image_trim_out\generating_binaries.py
 
 zephyr_rtos\zephyr-env.cmd
@@ -17,5 +18,5 @@ echo ZES_ENABLE_SYSMAN=%ZES_ENABLE_SYSMAN%
 echo ZEPHYR_TOOLCHAIN_VARIANT=%ZEPHYR_TOOLCHAIN_VARIANT%
 echo ZEPHYR_SDK_INSTALL_DIR=%ZEPHYR_SDK_INSTALL_DIR%
 echo ================= MCHP ENV ========================
-echo MEC172X_SPI_GEN=%MEC172X_SPI_GEN%
+echo MEC5_SPI_GEN=%MEC5_SPI_GEN%
 echo EC_IMG_GEN=%EC_IMG_GEN%
