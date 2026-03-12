1df068742282a36db72cf62079f10e384ad73c1c
MCHP-Andy <andy.chang@microchip.com>
2026-03-12 15:24:39 +0800

    feat(thermal): implement fan control

---
+++ app/driver/fan_qualcomm.c
@@ -0,0 +1,110 @@
+/*
+ * @Author: andy.chang
+ * @Date: 2025-06-29 17:06:01
+ * @Last Modified by: andy.chang
+ * @Last Modified time: 2026-03-12 14:16:19
+ */
+
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(fan_qualcomm, LOG_LEVEL_WRN);
+
+static const struct fan_dev_t {
+    const struct pwm_dt_spec fan;
+    const struct device *tach;
+} fan_dev_list[] = {
+    {
+        PWM_DT_SPEC_GET(DT_ALIAS(cpu_fan)),
+        DEVICE_DT_GET(DT_ALIAS(cpu_tach)),
+    },
+    {
+        PWM_DT_SPEC_GET(DT_ALIAS(base_fan)),
+        DEVICE_DT_GET(DT_ALIAS(base_tach)),
+    },
+};
+
+int app_fan_set_speed(int fan_id, uint8_t speed) {
+    if (fan_id >= ARRAY_SIZE(fan_dev_list)) {
+        LOG_ERR("Invalid fan ID: %d", fan_id);
+        return -EINVAL;
+    }
+
+    if (speed > 100) {
+        speed = 100; // Cap speed at 100%
+    }
+
+    int ret = 0;
+    uint32_t pulse = (uint64_t)fan_dev_list[fan_id].fan.period * speed / 100;
+
+    ret = pwm_set_pulse_dt(&fan_dev_list[fan_id].fan, pulse);
+    if (ret < 0) {
+        LOG_ERR("Failed to set speed for fan%d: %d", fan_id, ret);
+    } else {
+        LOG_INF("Set fan%d speed to %d%%", fan_id, speed);
+    }
+
+    return ret;
+}
+
+int app_fan_get_rpm(int fan_id, uint16_t *rpm) {
+    if (fan_id >= ARRAY_SIZE(fan_dev_list)) {
+        LOG_ERR("Invalid fan ID: %d", fan_id);
+        return -EINVAL;
+    }
+
+    int ret = 0;
+    const struct device *tach = fan_dev_list[fan_id].tach;
+    struct sensor_value val = {0};
+
+    ret = sensor_sample_fetch_chan(tach, SENSOR_CHAN_RPM);
+    if (ret) {
+        LOG_ERR("Failed to fetch RPM sample for fan%d: %d", fan_id, ret);
+        return ret;
+    }
+
+    ret = sensor_channel_get(tach, SENSOR_CHAN_RPM, &val);
+    if (ret) {
+        LOG_ERR("Failed to get RPM for fan%d: %d", fan_id, ret);
+        return ret;
+    }
+
+    *rpm = (uint16_t)val.val1;
+    LOG_INF("Fan%d RPM is %d", fan_id, *rpm);
+
+    return ret;
+}
+
+#include <zephyr/init.h>
+
+static int init_config(void) {
+    int ret = 0;
+
+    LOG_INF("Initializing thermal configuration...");
+
+    for (size_t i = 0; i < ARRAY_SIZE(fan_dev_list); i++) {
+
+        if (!pwm_is_ready_dt(&fan_dev_list[i].fan)) {
+            LOG_ERR("fan%d not ready", i);
+            return -ENODEV;
+        }
+
+        ret = pwm_set_pulse_dt(&fan_dev_list[i].fan, 0);
+        if (ret < 0) {
+            LOG_ERR("Failed to set PWM for fan%d: %d", i, ret);
+            return ret;
+        }
+
+        if (!device_is_ready(fan_dev_list[i].tach)) {
+            LOG_ERR("tach%d not ready", i);
+            return -ENODEV;
+        }
+    }
+
+    return ret;
+}
+
+SYS_INIT(init_config, APPLICATION, 0);
---
+++ app/service/thermal/service.c
@@ -0,0 +1,212 @@
+/*
+ * @Author: andy.chang 
+ * @Date: 2025-07-01 02:46:45 
+ * @Last Modified by: andy.chang
+ * @Last Modified time: 2026-03-12 15:17:46
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+// #include <interface/system.h>
+#include <interface/thermal.h>
+
+LOG_MODULE_REGISTER(thermal, LOG_LEVEL_INF);
+
+#define STACKSIZE 1024
+#define PRIORITY 7
+
+#define APP_FAN_SET_SPEED(fan_id, speed)                                       \
+    do {                                                                       \
+        int ret = app_fan_set_speed(fan_id, speed);                            \
+        if (ret < 0) {                                                         \
+            LOG_ERR("Failed to set fan%d speed: %d", fan_id, ret);             \
+        } else {                                                               \
+            LOG_INF("Fan%d speed set to %d%%", fan_id, speed);                 \
+        }                                                                      \
+    } while (0)
+
+enum {
+    THERMAL_STATE_IDLE = 0,
+    THERMAL_STATE_INITIAL,
+    THERMAL_STATE_RUNNING,
+    THERMAL_STATE_CLOSE,
+};
+
+typedef struct {
+    uint16_t thre;
+    uint8_t pwm;
+    uint8_t hyst;
+} fan_profile_t;
+
+static const fan_profile_t fan_profile[] = {
+    {.thre = 20, .pwm = 30, .hyst = 10},
+    {.thre = 40, .pwm = 45, .hyst = 10},
+    {.thre = 70, .pwm = 70, .hyst = 10},
+    {.thre = 85, .pwm = 100, .hyst = 0}, // No need hysteresis
+};
+
+static uint8_t fan_tbl_size = ARRAY_SIZE(fan_profile);
+static fan_profile_t *fan_tbl = (fan_profile_t *)fan_profile;
+static uint8_t pre_tmp[2];
+static uint16_t cur_tmp = 0;
+static K_SEM_DEFINE(thermal_sem, 0, 1);
+
+/**
+ * @brief Read the thermal sensors and update the current temperature
+ * 
+ */
+static inline void read_thermal_sensor(void) {
+    // TODO: Read the current temperature from sensors
+    cur_tmp = 45;
+}
+
+/**
+ * @brief Update fan speed based on the current temperature
+ *
+ * This function iterates through the fan profile table and sets the fan speed
+ * according to the current temperature. If no profile matches, it sets the fan
+ * speed to 0.
+ */
+static inline void fan_update(void) {
+    for (size_t idx = 0; idx < 2; idx++) {
+        int ret;
+        uint16_t rpm = 0;
+        bool pwm_updated = false;
+
+        for (int i = fan_tbl_size - 1; i >= 0; i--) {
+            uint16_t thre = fan_tbl[i].thre;
+            uint8_t pwm = fan_tbl[i].pwm;
+            uint8_t hyst = fan_tbl[i].hyst;
+
+            if (cur_tmp >= thre) {
+                if ((pre_tmp[idx] > cur_tmp) && hyst > 0) {
+                    if ((i + 1 < fan_tbl_size) &&
+                        ((fan_tbl[i + 1].thre - cur_tmp) < hyst)) {
+                        // keep origin PWM
+                    } else {
+                        APP_FAN_SET_SPEED(idx, pwm);
+                    }
+                } else {
+                    APP_FAN_SET_SPEED(idx, pwm);
+                }
+                pwm_updated = true;
+                break;
+            }
+        }
+
+        if (pwm_updated == false) {
+            // If no PWM updated, set to 0
+            APP_FAN_SET_SPEED(idx, 0);
+        }
+
+        pre_tmp[idx] = cur_tmp;
+
+        ret = app_fan_get_rpm(idx, &rpm);
+        if (ret < 0) {
+            LOG_ERR("Failed to get fan%d RPM: %d", idx, ret);
+        } else {
+            LOG_INF("Fan%d RPM is %d", idx, rpm);
+        }
+    }
+}
+
+static void service(void) {
+    uint8_t state = THERMAL_STATE_RUNNING; // THERMAL_STATE_IDLE;
+    k_timeout_t wait_time = K_MSEC(100);
+
+    k_sleep(wait_time);
+
+    // wait_time = K_FOREVER;
+    wait_time = K_MSEC(5000);
+
+    while (1) {
+
+        switch (state) {
+        case THERMAL_STATE_IDLE: // idle
+            LOG_INF("Thermal service is idle, waiting for initialization");
+            k_sem_take(&thermal_sem, wait_time);
+        case THERMAL_STATE_INITIAL: // initial
+            LOG_INF("Thermal service is initializing");
+            // TODO: Initialize thermal sensors and fans
+
+            // Reset previous temperature
+            memset(pre_tmp, 0xff, sizeof(pre_tmp));
+
+            state = THERMAL_STATE_RUNNING; // Change state to running
+            LOG_INF("Thermal service is running");
+        case THERMAL_STATE_RUNNING: // running
+            // Read the current temperature from TMP451 sensors
+            read_thermal_sensor();
+
+            // Update the fan speed based on the temperature
+            fan_update();
+
+            k_sem_take(&thermal_sem, wait_time);
+            break;
+
+        case THERMAL_STATE_CLOSE: // close
+            LOG_INF("Thermal service is closing");
+            state = THERMAL_STATE_IDLE; // Change state to idle
+            break;
+
+        default:
+            state = THERMAL_STATE_IDLE; // Change state to idle
+            break;
+        }
+
+        // TODO: check state change request, e.g. from system event or shell command
+    }
+}
+
+K_THREAD_DEFINE(thermal_id, STACKSIZE, service, NULL, NULL, NULL, PRIORITY, 0, 0);
+
+#ifdef CONFIG_SHELL
+#include <zephyr/shell/shell.h>
+
+static int cmd_fan_set(const struct shell *sh, size_t argc, char **argv) {
+    uint8_t ch = 0, duty = 0;
+
+    ch = atoi(argv[1]);
+    
+    duty = atoi(argv[2]);
+    if (duty > 100) {
+        duty = 100;
+    }
+
+    int ret = app_fan_set_speed(ch, duty);
+    if (ret < 0) {
+        shell_error(sh, "Failed to set fan%d speed: %d", ch, ret);
+    } else {
+        shell_info(sh, "Fan%d speed set to %d%%", ch, duty);
+    }
+    
+    return 0;
+}
+
+static int cmd_fan_get(const struct shell *sh, size_t argc, char **argv) {
+    uint8_t ch = 0;
+    uint16_t rpm = 0;
+
+    ch = atoi(argv[1]);
+
+    int ret = app_fan_get_rpm(ch, &rpm);
+    if (ret < 0) {
+        shell_error(sh, "Failed to get fan%d RPM: %d", ch, ret);
+    } else {
+        shell_info(sh, "Fan%d RPM is %d", ch, rpm);
+    }
+
+    return ret;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_fan,
+	SHELL_CMD_ARG(set, NULL,
+		"Set fan duty", cmd_fan_set, 2, 0),
+	SHELL_CMD_ARG(off, NULL,
+		"Get fan RPM", cmd_fan_get, 1, 0),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
+
+SHELL_CMD_REGISTER(fan, &sub_fan, "Fan commands", NULL);
+#endif
