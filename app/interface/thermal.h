a0e3f684ab2627d41c5253b0d5e8b6ca0435e801
MCHP-Andy <andy.chang@microchip.com>
2026-04-17 15:10:46 +0800

    feat(thermal): Implement thermal and fan interface

--- app/interface/thermal.h
+++ app/interface/thermal.h
@@ -2,13 +2,51 @@
  * @Author: andy.chang 
  * @Date: 2026-04-17 00:05:32 
  * @Last Modified by: andy.chang
- * @Last Modified time: 2026-04-17 03:32:05
+ * @Last Modified time: 2026-04-17 18:12:46
  */
 
 #pragma once
 
 #include <stdio.h>
 #include <stdint.h>
+
+typedef enum {
+    // Temperature source
+    THERM_SRC_CPU = 1,
+    THERM_SRC_SKIN = 2,
+    THERM_SRC_MAX,
+
+    // Fan ID
+    FAN_ID_1 = 1,
+    FAN_ID_2 = 2,
+    FAN_ID_MAX,
+
+    // Thermistors device
+    THERM_DEV_1 = 1,
+    THERM_DEV_2 = 2,
+    THERM_DEV_3 = 3,
+    THERM_DEV_MAX,
+
+    // FAN profile
+    FAN_PROFILE_BATTERY_SAVER = 1,
+    FAN_PROFILE_BETTER_BAT_CHG_IN = 2,
+    FAN_PROFILE_BETTER_BAT_CHG_OUT = 3,
+    FAN_PROFILE_BETTER_PERFORMANCE_CHG_IN = 4,
+    FAN_PROFILE_BETTER_PERFORMANCE_CHG_OUT = 5,
+    FAN_PROFILE_BEST_PERFORMANCE_CHG_IN = 6,
+    FAN_PROFILE_BEST_PERFORMANCE_CHG_OUT = 7,
+    FAN_PROFILE_MAX,
+
+    // FAN status
+    FAN_STA_OFF = 0,
+    FAN_STA_ON = 1,
+    FAN_STA_FAIL = 2,
+    FAN_STA_MAX,
+
+} therm_id_t;
+
+
+
 
 typedef struct fan_tbl_t{
     uint8_t rpm; // Unit in 100 RPM (Eg: 5 => 500 RPM)
@@ -17,7 +55,7 @@ typedef struct fan_tbl_t{
 } fan_tbl_t;
 
 typedef struct fan_ctrl_t{
-    uint8_t state; // 0x00: OFF, 0x01: ON, 0x02: Failure
+    therm_id_t state; // 0x00: OFF, 0x01: ON, 0x02: Failure
     uint16_t rpm; // Fan speed in RPM (Revolutions Per Minute)
     
     uint16_t trip_low; // Trip point low in the unit of RPM.
@@ -32,8 +70,8 @@ typedef struct fan_ctrl_t{
                      // 0x06: Best performance with charger plugged in
                      // 0x07: Best performance with charger plugged out
 
-    uint8_t lut[3]; // LUT number for 2 sources. 0: Dummy, 1: CPU, 2: Skin
-    fan_tbl_t *fan_tbl[3]; // Fan profile table pointer for 2 sources. 0: Dummy, 1: CPU, 2: Skin
+    uint8_t tbl_size; // LUT number for 2 sources. 0: Dummy, 1: CPU, 2: Skin
+    fan_tbl_t *fan_tbl[THERM_SRC_MAX]; // Fan profile table pointer for 2 sources. 0: Dummy, 1: CPU, 2: Skin
 
     uint8_t dbg_mode; // BIT0: 0-Normal, 1-Debug mode
                       // BIT1: 0-Fan off, 1-Fan on
@@ -54,21 +92,32 @@ typedef struct therm_dev_t {
     uint8_t crt; // deg C
 } therm_dev_t;
 
-typedef struct thermal_ctrl_t{
-    uint16_t temp[3]; // Temperature value in the unit of 0.1 deg C.(Eg: 651 => 65.1deg C)
-                      // 0: Dummy, 1: CPU, 2: Skin
+typedef struct therm_ctrl_t{
+    uint8_t temp_num; // Number of temperature sources
+    uint16_t *temp; // Temperature value in the unit of 0.1 deg C.(Eg: 651 => 65.1deg C)
+                    // 0: Dummy, 1: CPU, 2: Skin
+    
+    uint8_t fan_num; // Number of fans
+    fan_ctrl_t *fan_blk; // Fan control information. 0: Dummy, 1: CPU, 2: Skin
 
-    fan_ctrl_t fan_ctrl[3]; // Fan control information. 0: Dummy
-    therm_dev_t therm_dev[4]; // Thermal device information. 0: Dummy
+    uint8_t therm_num; // Number of thermistors
+    therm_dev_t *therm_blk; // Thermal device information. 0: Dummy, 1~4: Thermistor 0~3
 
     uint16_t adc_sample_ms; // ADC sample rate in the unit of ms (Min: 100ms)
-} thermal_ctrl_t;
+} therm_ctrl_t;
 
 
-int thermal_ctrl_get(thermal_ctrl_t *ctrl);
+int therm_tmp_get(therm_id_t tmp_src, uint16_t *tmp);
+int therm_tmp_set(therm_id_t tmp_src, uint16_t tmp);
 
-int thermal_ctrl_set(const thermal_ctrl_t *ctrl);
+int therm_fan_ctrl_get(therm_id_t fan_id, fan_ctrl_t *ctrl);
+int therm_fan_ctrl_set(therm_id_t fan_id, const fan_ctrl_t *ctrl);
 
-int board_lut_get(uint8_t profile, uint8_t fan_id, uint8_t tmp_src,
+int therm_sensor_blk_get(therm_id_t dev_id, therm_dev_t *blk);
+int therm_sensor_blk_set(therm_id_t dev_id, const therm_dev_t *blk);
+
+int therm_adc_sample_rate_get(uint16_t *ms);
+int therm_adc_sample_rate_set(uint16_t ms);
+
+int therm_tbl_get(therm_id_t profile, therm_id_t fan_id, therm_id_t tmp_src,
                   fan_tbl_t **tbl, uint8_t *len);
-                  
---
+++ app/service/thermal/profile_table.c
@@ -0,0 +1,324 @@
+
+#include <zephyr/kernel.h>
+#include <interface/thermal.h>
+
+// Profile1: Battery saver
+static fan_tbl_t p1_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p1_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p1_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p1_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+// Profile2: Better performance with charger plugged in
+static fan_tbl_t p2_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p2_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p2_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p2_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+// Profile3: Better performance with charger plugged out
+static fan_tbl_t p3_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p3_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p3_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p3_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+// Profile4: Better performance with charger plugged in
+static fan_tbl_t p4_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p4_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p4_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p4_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+// Profile5: Better performance with charger plugged out
+static fan_tbl_t p5_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p5_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p5_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p5_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+// Profile6: Best performance with charger plugged in
+static fan_tbl_t p6_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p6_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p6_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p6_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+// Profile7: Best performance with charger plugged out
+static fan_tbl_t p7_fan1_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p7_fan1_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p7_fan2_cpu_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+static fan_tbl_t p7_fan2_skin_lut[] = {
+    {.rpm = 0x00, .temp_high = 0x46, .temp_low = 0x00},
+    {.rpm = 0x22, .temp_high = 0x50, .temp_low = 0x43},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0x4D},
+    {.rpm = 0x34, .temp_high = 0xFF, .temp_low = 0xFF},
+};
+
+int therm_tbl_get(therm_id_t profile, therm_id_t fan_id, therm_id_t tmp_src,
+                  fan_tbl_t **tbl, uint8_t *len) {
+    if (profile == 0 || profile > 7 || fan_id == 0 || fan_id > 2 ||
+        tmp_src == 0 || tmp_src > 2 || tbl == NULL || len == NULL) {
+        return -EINVAL;
+    }
+
+    uint32_t codec = (profile << 16) | (fan_id << 8) | tmp_src;
+
+    switch (codec) {
+    case 0x010101:
+        *tbl = p1_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p1_fan1_cpu_lut);
+        break;
+    case 0x010102:
+        *tbl = p1_fan1_skin_lut;
+        *len = ARRAY_SIZE(p1_fan1_skin_lut);
+        break;
+    case 0x010201:
+        *tbl = p1_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p1_fan2_cpu_lut);
+        break;
+    case 0x010202:
+        *tbl = p1_fan2_skin_lut;
+        *len = ARRAY_SIZE(p1_fan2_skin_lut);
+        break;
+
+    case 0x020101:
+        *tbl = p2_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p2_fan1_cpu_lut);
+        break;
+    case 0x020102:
+        *tbl = p2_fan1_skin_lut;
+        *len = ARRAY_SIZE(p2_fan1_skin_lut);
+        break;
+    case 0x020201:
+        *tbl = p2_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p2_fan2_cpu_lut);
+        break;
+    case 0x020202:
+        *tbl = p2_fan2_skin_lut;
+        *len = ARRAY_SIZE(p2_fan2_skin_lut);
+        break;
+    
+    case 0x030101:
+        *tbl = p3_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p3_fan1_cpu_lut);
+        break;
+    case 0x030102:
+        *tbl = p3_fan1_skin_lut;
+        *len = ARRAY_SIZE(p3_fan1_skin_lut);
+        break;
+    case 0x030201:
+        *tbl = p3_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p3_fan2_cpu_lut);
+        break;
+    case 0x030202:
+        *tbl = p3_fan2_skin_lut;
+        *len = ARRAY_SIZE(p3_fan2_skin_lut);
+        break;
+
+    case 0x040101:
+        *tbl = p4_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p4_fan1_cpu_lut);
+        break;
+    case 0x040102:
+        *tbl = p4_fan1_skin_lut;
+        *len = ARRAY_SIZE(p4_fan1_skin_lut);
+        break;
+    case 0x040201:
+        *tbl = p4_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p4_fan2_cpu_lut);
+        break;
+    case 0x040202:
+        *tbl = p4_fan2_skin_lut;
+        *len = ARRAY_SIZE(p4_fan2_skin_lut);
+        break;
+
+    case 0x050101:
+        *tbl = p5_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p5_fan1_cpu_lut);
+        break;
+    case 0x050102:
+        *tbl = p5_fan1_skin_lut;
+        *len = ARRAY_SIZE(p5_fan1_skin_lut);
+        break;
+    case 0x050201:
+        *tbl = p5_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p5_fan2_cpu_lut);
+        break;
+    case 0x050202:
+        *tbl = p5_fan2_skin_lut;
+        *len = ARRAY_SIZE(p5_fan2_skin_lut);
+        break;
+
+    case 0x060101:
+        *tbl = p6_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p6_fan1_cpu_lut);
+        break;
+    case 0x060102:
+        *tbl = p6_fan1_skin_lut;
+        *len = ARRAY_SIZE(p6_fan1_skin_lut);
+        break;
+    case 0x060201:
+        *tbl = p6_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p6_fan2_cpu_lut);
+        break;
+    case 0x060202:
+        *tbl = p6_fan2_skin_lut;
+        *len = ARRAY_SIZE(p6_fan2_skin_lut);
+        break;
+
+    case 0x070101:
+        *tbl = p7_fan1_cpu_lut;
+        *len = ARRAY_SIZE(p7_fan1_cpu_lut);
+        break;
+    case 0x070102:
+        *tbl = p7_fan1_skin_lut;
+        *len = ARRAY_SIZE(p7_fan1_skin_lut);
+        break;
+    case 0x070201:
+        *tbl = p7_fan2_cpu_lut;
+        *len = ARRAY_SIZE(p7_fan2_cpu_lut);
+        break;
+    case 0x070202:
+        *tbl = p7_fan2_skin_lut;
+        *len = ARRAY_SIZE(p7_fan2_skin_lut);
+        break;
+
+    default:
+        *tbl = NULL;
+        *len = 0;
+        return -EINVAL;
+        break;
+    }
+
+    return 0;
+}
