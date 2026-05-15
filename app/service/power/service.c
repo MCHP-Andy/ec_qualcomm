4a8dc97622764ecac6f7911cb25fcb431e3dfded
MCHP-Andy <andy.chang@microchip.com>
2026-05-15 16:28:16 +0800

    refactor(power): Refine power change flow

--- app/service/power/service.c
+++ app/service/power/service.c
@@ -15,30 +15,33 @@
 
 LOG_MODULE_REGISTER(power, CONFIG_POWER_LOG_LEVEL);
 
+enum {
+    PWR_EVT_STA_UPDATE = LOCAL_EVT_START,
+};
+
+#define PWR_STA_UPDATE BIT(PWR_EVT_STA_UPDATE)
+
 static K_EVENT_DEFINE(event);
 SYS_EVENT_SUBSCRIBE(power, event);
 static pwr_sta_t pwr_state = PWR_STA_G3;
+static pwr_sta_t next_pwr_state = PWR_STA_G3;
 
 static const char *pwr_state_str[] = {
-	[PWR_STA_S0] = "S0",
-	[PWR_STA_MS] = "Modern Standby",
-	[PWR_STA_S3] = "S3",
-	[PWR_STA_S4] = "S4",
-	[PWR_STA_S5] = "S5",
-	[PWR_STA_G3] = "G3",
+    [PWR_STA_S0] = "S0", [PWR_STA_MS] = "Modern Standby",
+    [PWR_STA_S3] = "S3", [PWR_STA_S4] = "S4",
+    [PWR_STA_S5] = "S5", [PWR_STA_G3] = "G3",
 };
 
-static const char *pwr_state_to_str(pwr_sta_t state)
-{
-	if (state <= 0 || state >= PWR_STA_MAX || pwr_state_str[state] == NULL) {
-		return "Unknown";
-	}
-	return pwr_state_str[state];
+static const char *pwr_state_to_str(pwr_sta_t state) {
+    if (state <= 0 || state >= PWR_STA_MAX || pwr_state_str[state] == NULL) {
+        return "Unknown";
+    }
+    return pwr_state_str[state];
 }
 
 int pwr_state_get(pwr_sta_t *state) {
     if (state == NULL) {
-        LOG_WRN("Invalid state pointer");
+        LOG_ERR("Invalid state pointer");
         return -EINVAL;
     }
 
@@ -51,21 +54,19 @@ int pwr_state_get(pwr_sta_t *state) {
 
 int pwr_state_set(pwr_sta_t state) {
     if (state >= PWR_STA_MAX || state < PWR_STA_S0) {
-        LOG_WRN("Invalid power state: %d", state);
+        LOG_ERR("Invalid power state: %d", state);
         return -EINVAL;
     }
 
     if (state == pwr_state) {
-        LOG_DBG("Power state already at %s", pwr_state_to_str(state));
+        LOG_WRN("Power state already at %s", pwr_state_to_str(state));
         return 0;
     }
 
-    pwr_state = state;
+    next_pwr_state = state;
 
-    LOG_INF("Changed power state: %s", pwr_state_to_str(state));
-
-    // Notify other modules of power state change
-    SYS_EVENT_SUBMIT(SYS_PWR_STA_CHG);
+    // Notify service thread to process power state change
+    k_event_post(&event, PWR_STA_UPDATE);
 
     return 0;
 }
@@ -75,12 +76,25 @@ static void service(void) {
 
     while (1) {
         // Get event
-        evt = k_event_wait(&event, (SYS_PWR_STA_CHG), true, K_FOREVER);
+        evt = k_event_wait(&event, (SYS_EVT_MASK | PWR_STA_UPDATE), true,
+                           K_FOREVER);
 
-        if (evt & SYS_PWR_STA_CHG) {
+        if (evt & PWR_STA_UPDATE) {
             pwr_sta_t state = 0;
             pwr_state_get(&state);
+
+            LOG_INF("Prepare to change power state: %s -> %s",
+                    pwr_state_to_str(state), pwr_state_to_str(next_pwr_state));
+
+            // TODO: power process depend on platform
+            // ...
+
+            pwr_state = next_pwr_state;
+            pwr_state_get(&state);
             LOG_INF("Power state: %s", pwr_state_to_str(state));
+
+            // Notify other modules of power state change
+            SYS_EVENT_SUBMIT(SYS_PWR_STA_CHG);
         }
     }
 }
@@ -115,7 +129,8 @@ static int cmd_pwr_set(const struct shell *sh, size_t argc, char **argv) {
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_pwr,
     SHELL_CMD_ARG(get, NULL, "Get current power state", cmd_pwr_get, 1, 0),
-    SHELL_CMD_ARG(set, NULL, "Set power state <state_id>", cmd_pwr_set, 2, 0),
+    SHELL_CMD_ARG(set, NULL, "Set power state <state_id>\n"
+                             "1: S0, 2: Modern Standby, 3: S3, 4: S4, 5: S5, 6: G3", cmd_pwr_set, 2, 0),
     SHELL_SUBCMD_SET_END
 );
 
